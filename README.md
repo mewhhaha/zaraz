09a3bba0b171000baf6180aa1494cd84c65865e1
Jacob Torrång <jacob.torrang@pagero.com>
2024-05-21 14:14:25 +0200

    chore: move up everything to top

---
+++ README.md
@@ -0,0 +1,10 @@
+# Zaraz app
+A private todo app.
+
+- Framework: Remix
+- Styling: Tailwindcss
+- Database: Single D1 database
+- Auth: OAuth with Google
+- Host: Cloudflare
+
+![screenshot of the app](./docs/screenshot.png)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      boxShadow: {
-        brutalist: "2px 2px 0px rgba(0,0,0,1)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
