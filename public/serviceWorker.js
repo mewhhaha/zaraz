3c24764749b3aaea16516bcc92e22a26b5b5206a
mewhhaha <kopatheonlyone@hotmail.com>
2024-06-03 22:26:39 +0200

    use swr in service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,24 @@
-self.addEventListener("fetch", function () {
-  return;
+// Establish a cache name
+const cacheName = "v1";
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.destination === "image") {
+    event.respondWith(
+      caches.open(cacheName).then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            },
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      }),
+    );
+  } else {
+    return;
+  }
 });
